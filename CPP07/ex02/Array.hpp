542ff264ee8109b55feb8c6f6a0c24e10619731e
Devsstudent <devsstudent777@gmail.com>
2023-03-07 23:58:46 +0100

    CPP07 almost done

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,42 +1,41 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <exception>
+
 template <class T>
 class	Array {
 	private:
 		T	*arr;
+		int	_size;
 	public:
-		Array(void);
-		Array(int n);
-		Array(const Array &a);
-		~Array(void);
-		int	size(void);
+		Array(void): arr(new T[0]), _size(0){};
+		Array(int n): arr(new T[n]), _size(n){
+			for (int i = 0; i < _size; i++)
+				arr[i] = 0;
+	};
+		Array(const Array &a): arr(new T[a.size()]), _size(a.size())
+		{
+			for (int i = 0; i < a.size(); i++)
+				arr[i] = a.arr[i];
+		};
+		~Array(void) {
+			delete[] arr;
+		};
+		int	size(void) const
+		{
+			return (_size);
+		};
+		T &operator[](const int idx)
+		{
+			if (idx > size() - 1 || idx < 0)
+				throw (OutOfBounds());
+			return (arr[idx]);
+		};
+		class OutOfBounds: public std::exception {
+			public:
+				OutOfBounds(void){};
+				virtual const char * what() const throw (){ return ("Index is out of Bounds");};
+	};
 };
-
-Array::Array(void): arr(new T[0])
-{
-}
-
-Array::Array(int n) : arr(new T[n])
-{
-}
-
-Array::Array(const Array &a): arr(new T[a.size()])
-{
-	//try and catch ?
-	for (int i = 0; i < a.size(); i++)
-		arr[i] = a.arr[i];
-	//size
-}
-
-Array::~Array(void)
-{
-	delete[] arr;
-}
-
-int	Array::size(void)
-{
-	//try and catch ?
-}
-
 #endif
