c30b05282a40431474e79e0f78290fd001ae1aef
Devsstudent <devsstudent777@gmail.com>
2023-03-08 21:23:56 +0100

    CPP08 ex01 done insh

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:41:29 by odessein          #+#    #+#             */
-/*   Updated: 2023/03/08 16:12:04 by odessein         ###   ########.fr       */
+/*   Updated: 2023/03/08 21:23:25 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ Span::Span(int	_n): filled(0)
 
 Span::Span(Span const &rhs)
 {
-	this = rhs;
+	*this = rhs;
 }
 
 // Destructor
@@ -44,13 +44,88 @@ Span::~Span(void)
 Span	&Span::operator=(Span const &rhs)
 {
 	n = rhs.n;
+	filled = rhs.filled;
 	arr.clear();
+	arr = rhs.arr;
 	return (*this);
 }
 
 // Methods
-void	addNumber(int nb)
+void	Span::addNumber(int nb)
 {
 	if (filled == n)
-		std::cout << "Cannot add more numbers" << std::endl;
+		throw (Exception_out());
+	filled++;
+	arr.push_back(nb);
+}
+
+void	Span::addManyNumbers(int *arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		try {
+			addNumber(arr[i]);
+		}
+		catch (Exception_out& exception) {
+			std::cerr << exception.what() << std::endl;
+		}
+	}
+}
+
+int	Span::shortestSpan(void)
+{
+	std::vector<int>::iterator	it;
+	std::vector<int>::iterator	it2;
+	int							buff;
+	int							max_val;
+	bool						same;
+
+
+	it = std::max_element(arr.begin(), arr.end());
+	max_val = *it;
+	if (filled <= 1)
+		throw (Exception_not_enough());
+	for (it = arr.begin(); it != arr.end(); ++it)
+	{
+		same = false;
+		buff = max_val;
+		for (it2 = arr.begin(); it2 != arr.end(); ++it2)
+		{
+			if (*it == *it2)
+				same = true;
+			else if (*it == *it2 && same)
+				return (0);
+			else if (abs(*it - *it2) < buff)
+				buff = abs(*it - *it2);
+		}
+	}
+	return (buff);
+}
+
+const char	*Span::Exception_out::what() const throw()
+{
+	return ("Not possible to add element");
+}
+
+const char	*Span::Exception_not_enough::what() const throw()
+{
+	return ("Not possible to search span with less than 2 elem :'(");
+}
+
+int	Span::longestSpan(void)
+{
+	std::vector<int>::iterator	it;
+	int							max_val;
+	int							min_val;
+
+	if (filled <= 1)
+		throw (Exception_not_enough());
+	it = std::min_element(arr.begin(), arr.end());
+	min_val = *it;
+	it = std::max_element(arr.begin(), arr.end());
+	max_val = *it;
+	if (min_val == max_val)
+		return (0);
+	else
+		return (max_val - min_val);
 }
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:41:22 by odessein          #+#    #+#             */
-/*   Updated: 2023/03/08 16:00:49 by odessein         ###   ########.fr       */
+/*   Updated: 2023/03/08 21:18:49 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 # define SPAN_HPP
 
 # include <vector>
+# include <iostream>
+# include <algorithm>
+# include <cmath>
 
 class Span
 {
 	private:
 		unsigned int		n;
-		int					filled;
+		unsigned int		filled;
 		std::vector<int>	arr;
 	// Attributes
 		
@@ -29,6 +32,7 @@ class Span
 	public:
 	// Constructors
 		Span(void);
+		Span(int _n);
 		Span(Span const &rhs);
 
 	// Destructor
@@ -42,6 +46,21 @@ class Span
 		Span	&operator=(Span const &rhs);
 
 	// Methods
+	int		longestSpan(void);
+	int		shortestSpan(void);
+	void	addNumber(int nb);
+	void	addManyNumbers(int *arr, int size);
+
+	class	Exception_out: public std::exception
+	{
+		public:
+			virtual const char * what() const throw();
+	};
+	class	Exception_not_enough: public std::exception
+	{
+		public:
+			virtual const char * what() const throw();
+	};
 };
 
 #endif
